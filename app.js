36b419145e9d3b79a188af6e2d43c4aad5971412
kirasys <kirasys0@naver.com>
2019-09-13 06:37:58 +0000

    first commint

---
+++ app.js
@@ -0,0 +1,27 @@
+// Load env setting
+require('dotenv').config();
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+var app = express();
+var PORT = process.env.SERVER_PORT;
+
+// Static File Service
+app.use(express.static('public'));
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Use ES6 native Promise
+mongoose.Promise = global.Promise
+
+// Connect to Mongodb server
+mongoose.connect(process.env.MONGO_URL)
+	.then(() => console.log('Successfully connected to mongodb.'))
+	.catch(e => console.error(e));
+
+var server = app.listen(PORT, function() {
+	console.log(`Server listening on ${PORT}`);
+});
