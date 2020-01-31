2431457a61045a97cd5b38399c7fcb87b0f677af
너굴이 <hancomlinux@naver.com>
2020-01-31 19:51:28 +0000

    users auth 생략

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const auth = require("../../../middlewares/auth");
 
-router.use("/", auth.login);
+// router.use("/", auth.login);
 router.post("/", require("./createUser"));
 router.delete("/", require("./deleteUser"));
 router.get("/", require("./getUsers"));
