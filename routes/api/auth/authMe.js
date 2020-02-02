3e87da7738b71d2bc921f76d93120688719b9f84
너굴이 <hancomlinux@naver.com>
2020-02-02 13:11:47 +0000

    data 객체 안 user 프로퍼티로 넘겨주기로 수정

--- routes/api/auth/authMe.js
+++ routes/api/auth/authMe.js
@@ -8,18 +8,20 @@ module.exports = async (req, res) => {
     let user = await User.findOneById(req.token.userId);
     if (!user) throw new Error("해당 유저는 존재하지 않습니다.");
     let data = {
-      id: user.dataValues.id,
-      username: user.dataValues.username,
-      type: user.dataValues.type
+      user: {
+        id: user.dataValues.id,
+        username: user.dataValues.username,
+        type: user.dataValues.type
+      }
     };
     if (user.dataValues.type === "student") {
       let student = await Student.findOneByUserId(user.dataValues.id);
       if (!student) throw new Error("해당 유저의 정보가 올바르지 않습니다.");
-      data.student = student.dataValues;
+      data.user.student = student.dataValues;
     } else if (user.dataValues.type === "teacher") {
       let teacher = await Teacher.findOneByUserId(user.dataValues.id);
       if (!teacher) throw new Error("해당 유저의 정보가 올바르지 않습니다.");
-      data.teacher = teacher.dataValues;
+      data.user.teacher = teacher.dataValues;
     }
     res.json({
       success: true,
