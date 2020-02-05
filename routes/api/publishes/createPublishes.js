262d46b47753f4eef2a72d7d429e5af911e4c5eb
너굴이 <hancomlinux@naver.com>
2020-02-05 18:33:49 +0000

    모든 타입에 대해 생성된 publishes 리턴

--- routes/api/publishes/createPublishes.js
+++ routes/api/publishes/createPublishes.js
@@ -45,32 +45,29 @@ module.exports = async (req, res) => {
       })
     );
     
-    if (type === "workpaper") {
-      let data = {};
-      let publish = publishes[0];
-      data.publish = publish.dataValues;
-      let collection = await Collection.findOneById(publish.dataValues.collectionId);
-      if (!collection) throw new Error("존재하지 않는 컬렉션입니다.");
-      data.publish[publish.dataValues.collectionType] = collection.dataValues;
-      let collection_problems = await CollectionProblem.listProblemIdByCollectionId(publish.dataValues.collectionId);
-      data.publish[publish.dataValues.collectionType].problems = await Promise.all(collection_problems.map(async(r) => {
-        let problem = await Problem.findOneById(r.dataValues.problemId);
-        if (!problem) throw new Error("해당 문제를 찾을 수 없습니다.");
-        return problem.dataValues;
-      }));
-      res.json({
-        success: true,
-        message: "Publish를 생성했습니다.",
-        ecode: 200,
-        data: data
-      });
-    } else {
-      res.json({
-        success: true,
-        message: "Publish를 생성했습니다.",
-        ecode: 200
-      });
-    }
+    let collection_problems = await CollectionProblem.listProblemIdByCollectionId(collection.dataValues.id);
+    let problems = await Promise.all(collection_problems.map(async(r) => {
+      let problem = await Problem.findOneById(r.dataValues.problemId);
+      if (!problem) throw new Error("해당 문제를 찾을 수 없습니다.");
+      return problem.dataValues;
+    }));
+    
+    let data = {};
+    data.publishes = await Promise.all(
+      publishes.map(publish => {
+        let item = publish.dataValues;
+        item[collection.dataValues.type] = collection.dataValues;
+        item.problems = problems;
+        return item;
+      })
+    );
+    
+    res.json({
+      success: true,
+      message: "Publish를 생성했습니다.",
+      ecode: 200,
+      data: data
+    });
   } catch (error) {
     res.json({
       success: false,
